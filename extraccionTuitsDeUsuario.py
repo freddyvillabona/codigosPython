59a010d6c11998360a4e0deda8170ecbf88c91fe
freddyvillabona <36832437+freddyvillabona@users.noreply.github.com>
2018-03-18 19:34:56 -0400

    Add files via upload

---
+++ extraccionTuitsDeUsuario.py
@@ -0,0 +1,51 @@
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+
+#Credenciales de API TWITTER
+consumer_key = " "
+consumer_secret = " "
+access_key = " "
+access_secret = " "
+
+
+def get_all_tweets(screen_name):
+    
+    #3205 Tweets
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+
+    alltweets = []  
+
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+    alltweets.extend(new_tweets)
+
+    oldest = alltweets[-1].id - 1
+
+     while len(new_tweets) > 0:
+        print "getting tweets before %s" % (oldest)
+
+          new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+          alltweets.extend(new_tweets)
+
+          oldest = alltweets[-1].id - 1
+
+        print "...%s tweets downloaded so far" % (len(alltweets))
+
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+
+    # Guardadndo en archivo csv 
+    with open('%s_tweets.csv' % screen_name, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(["ID","Fecha","Texto"])
+        writer.writerows(outtweets)
+
+    pass
+
+if __name__ == '__main__':
+    #Usuario de Twitter
+    get_all_tweets("nicolasmaduro")
+
